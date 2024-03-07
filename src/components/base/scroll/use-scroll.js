2aaea9b9ca1d8fef01f36a4200dcbc0070832d9c
545龙哥哥 <18829188893@163.com>
2024-03-07 20:23:34 +0800

    歌手列表，歌手索引手指触达

--- src/components/base/scroll/use-scroll.js
+++ src/components/base/scroll/use-scroll.js
@@ -5,16 +5,24 @@ import ObserveDOM from "@better-scroll/observe-dom";
 // 自动判断内容变化，计算高度并滚动
 BScroll.use(ObserveDOM)
 
-export default function useScroll(wrapperRef, options) {
+export default function useScroll(wrapperRef, options, emit) {
   const scroll = ref(null)
-
   onMounted(() => {
-    scroll.value = new BScroll(wrapperRef.value, {
+    const scrollVal = scroll.value = new BScroll(wrapperRef.value, {
       observeDOM: true,
       ...options
     });
+    if(options.probeType > 0) {
+      scrollVal.on('scroll', pos => {
+        emit('scroll', pos)
+      })
+    }
   })
   onUnmounted(() => {
     scroll.value.destroy()
   })
+
+  return {
+    scroll
+  }
 }
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers" />
   </div>
 </template>
+<script>
+import { getSingerList } from '@/service/singer.js';
+import indexList from '../components/base/index-list/index-list.vue';
+
+export default {
+  name: 'singer',
+  async created() {
+    const result = await getSingerList();
+    console.log(result)
+    this.singers = result.singers;
+  },
+  components: {
+    indexList
+  },
+  data() {
+    return {
+      singers: []
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+ .singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+ }
+</style>
